5adc7510fe2da45ac47e903e7d5d936679f94be3
franksymon <frank.urbina.dev@gmail.com>
2024-05-21 20:24:12 -0400

    [ADD] se crea funcion global para el manejo de errores

---
+++ backend/controller/errorsController.ts
@@ -0,0 +1,11 @@
+export const globalErrorHandler = (err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const status = err.status || 'fail';
+  
+    res.status(statusCode).json({
+      status,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+    });
+  };
