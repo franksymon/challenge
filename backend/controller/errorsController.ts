a717f540afcc13fcab98361db74543939cbec41d
franksymon <frank.urbina.dev@gmail.com>
2024-05-23 14:52:21 -0400

    [UPD] se corrige manejador de errores global

--- backend/controller/errorsController.ts
+++ backend/controller/errorsController.ts
@@ -1,11 +1,15 @@
-export const globalErrorHandler = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const status = err.status || 'fail';
-  
-    res.status(statusCode).json({
-      status,
-      message: err.message,
-      error: err,
-      stack: err.stack,
-    });
-  };
+import { Request, Response, NextFunction } from 'express';
+import { AppError } from '../util/appError';
+
+export const globalErrorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.statusCode || 500;
+  const status = err.status || 'fail';
+
+  res.status(statusCode).json({
+    status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
--- backend/util/appError.ts
+++ backend/util/appError.ts
@@ -1,11 +1,14 @@
-// export class AppError extends Error {
-//     constructor(message, statusCode) {
-//       super(message);
-  
-//       this.message = message;
-//       this.statusCode = statusCode;
-//       this.status = `${statusCode}`.startsWith('4') ? 'error' : 'fail';
-  
-//       Error.captureStackTrace(this, this.constructor);
-//     }
-//   }
+export class AppError extends Error {
+  statusCode: number;
+  status: string;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+
+    // Captura la pila de llamadas para facilitar la depuración
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
