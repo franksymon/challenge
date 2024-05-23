99564496e4f50216a66a26190cf6f2bf986b7075
franksymon <frank.urbina.dev@gmail.com>
2024-05-23 14:55:03 -0400

    [UPD] se actulizan archivos init app

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
 import { db } from "./util/database";
+import { app } from "./app";
+import swaggerDocs from "./config/swagger";
 
-
-// initialize the express server
-export const app = express();
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? '8080';
 
 // Authenticate database credentials
 db.authenticate()
@@ -16,12 +15,9 @@ db.sync()
   .then(() => console.log('Database synced'))
   .catch(err => console.log(err));
 
-
-app.get('/healthcheck', (req, res) => {
-    res.send('¡Okay!');
-});
-
+// Start express server
 app.listen(PORT, () => {
     console.log(`Server Express is listening on port ${PORT}`);
+    swaggerDocs(app, PORT);
 });
 
